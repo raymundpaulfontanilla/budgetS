b0fd568b35218c22defeec960db9be98bd015e95
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-01-21 19:17:52 +0800

    added a dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,20 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col-md-12 p-5">
+            <img src="{{URL('images/avatar.png')}}" alt="profile Pic" height="200" width="200"><h2>Welcome back, {{ Auth::user()->name }}</h2>
+            <p class="text-muted">This is your dashboard. It gives you an overview of everything.</p>
         </div>
     </div>
 </div>
