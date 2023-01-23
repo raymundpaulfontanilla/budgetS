3ec121c86578cce3522a0c79a49c57601617f1f5
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-01-24 07:07:38 +0800

    hide scrollbar implementation

--- resources/views/layouts/pages/home.blade.php
+++ resources/views/layouts/pages/home.blade.php
@@ -1,11 +1,16 @@
 @extends('layouts/components/Navbar.navbar')
 
 @section('content')
-<div class="container fixed-top">
-    <div class="container m-4 p-4">
-      <div class="row">
-        <h1>Home</h1>
-      </div>
-    </div>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body class="overflow-hidden">
+  
+</body>
+</html>
 @endsection
