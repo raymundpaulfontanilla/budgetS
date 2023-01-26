a16c22e87102151f9e8e24d7fb91e9948a436fc0
gelai05 <angelamaemorales1105@gmail.com>
2023-01-26 23:20:01 +0800

    overview table

--- resources/views/dashboard/dpages/overview.blade.php
+++ resources/views/dashboard/dpages/overview.blade.php
@@ -1,7 +1,24 @@
 @extends('layouts.maindashboard')
 
 @section('content')
-<div>
-<h1>OVERVIEW PAGE</h1>
+<div class="container">
+    <table class="table mw-50">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Amount</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">Salary</th>
+      <td>June 15th</td>
+      <td>10,000</td>
+    </tr>
+    
+    </tr>
+  </tbody>
+</table>
 </div>
 @endsection
