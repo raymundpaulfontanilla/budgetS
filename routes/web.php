9aacee186b5a5921cf5cad644a582605143f6285
gelai05 <angelamaemorales1105@gmail.com>
2023-01-26 22:06:00 +0800

    commenting some routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -14,14 +14,18 @@ use App\Http\Controllers\HomeController;
 |
 */
 
+// Route::get('/', function () {
+//     return redirect()->route('layouts.maindashboard');
+// });
+
+// Auth::routes();
+
+// Route::get('/home', [HomeController::class, 'index'])->name('home');
+// Route::get('/expenses', [HomeController::class, 'expenses'])->name('expenses');
+// Route::get('/income', [HomeController::class, 'income'])->name('income');
+// Route::get('/transact', [HomeController::class, 'transact'])->name('transact');
+
+
 Route::get('/', function () {
-    return redirect()->route('login');
-});
-
-Auth::routes();
-
-Route::get('/home', [HomeController::class, 'index'])->name('home');
-Route::get('/expenses', [HomeController::class, 'expenses'])->name('expenses');
-Route::get('/income', [HomeController::class, 'income'])->name('income');
-Route::get('/transact', [HomeController::class, 'transact'])->name('transact');
-
+    return view('layouts.maindashboard');
+})->name('maindashboard');
