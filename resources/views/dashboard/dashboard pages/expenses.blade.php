4258e9dbab08c504ae51d88b8d25f4c4bf9fb88a
gelai05 <angelamaemorales1105@gmail.com>
2023-01-26 22:11:11 +0800

    expense page

--- resources/views/dashboard/dashboard pages/expenses.blade.php
+++ resources/views/dashboard/dashboard pages/expenses.blade.php
@@ -1,16 +1,7 @@
-@extends('layouts/components/Navbar.navbar')
+@extends('layouts/maindashboard')
 
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-  
-</body>
-</html>
+<div>
+  EXPENSE PAGE
+</div>
 @endsection
