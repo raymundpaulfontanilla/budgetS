ebfc50c6042239a0bdd089b79be73693bf0ab06a
Zafe123 <scottpilgrim712@gmail.com>
2023-01-28 20:22:52 +0800

    Implemented Modal to Income and Expense page

--- resources/views/dashboard/dpages/overview.blade.php
+++ resources/views/dashboard/dpages/overview.blade.php
@@ -12,6 +12,7 @@
   <link rel="stylesheet" href="{{ asset('homeblade.css') }}">
   <link rel="stylesheet" href="{{ asset('table.css') }}">
   <script src="https://cdn.tailwindcss.com"></script>
+
   <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
   <link href="https://afeld.github.io/emoji-css/emoji.css" rel="stylesheet">
@@ -23,10 +24,12 @@
 
 <body>
 
-  <h1 class="text-2xl">Hello $USERNAME</h1>
-  <p>Daily Report- January 23, 2023 11:00:56</p>
+  <h1 class="text-2xl ml-6">Hello $USERNAME</h1>
+  <p class="ml-6">Daily Report- January 23, 2023 11:00:56</p>
   <div class="flex flex-wrap">
+
     <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+
       <!--Metric Card-->
       <div class=" bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl
       p-5">
@@ -52,7 +55,8 @@
           </div>
           <div class="flex-1 text-right md:text-center">
             <h5 class="font-bold uppercase text-gray-600">Total Expenses</h5>
-            <h3 class="font-bold text-3xl">1000 <span class="text-red-500"><i class="fas fa-caret-up"></i></span></h3>
+            <h3 class="font-bold text-3xl">1000 <span class="text-red-500"></span></h3>
+            {{-- <i class="fas fa-caret-up"></i> --}}
           </div>
         </div>
       </div>
@@ -67,7 +71,8 @@
           </div>
           <div class="flex-1 text-right md:text-center">
             <h5 class="font-bold uppercase text-gray-600">Total Budget</h5>
-            <h3 class="font-bold text-3xl">14000 <span class="text-green-500"><i class="fas fa-caret-up"></i></span>
+            <h3 class="font-bold text-3xl">14000 <span class="text-green-500"></span>
+              {{-- <i class="fas fa-caret-up"></i> --}}
             </h3>
           </div>
         </div>
@@ -76,7 +81,7 @@
     <div class="w-full md:w-1/2 xl:w-1/3">
       {{-- Chart --}}
       <div class="shadow-lg rounded-lg ml-5">
-        <h1 class="py-3 px-5 bg-emerald-400">DATA REPORT
+        <h1 class="py-3 px-5 ">DATA REPORT
         </h1>
         <canvas class="p-10 " id="chartPie"></canvas>
       </div>
@@ -111,8 +116,8 @@
 
     <div class="container w-full md:w-4/5 xl:w-3/5  mx-auto px-2">
       <!--Card-->
-      <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
-        <table id="example" class="stripe hover" style="width:100%; padding-top: 1em;  padding-bottom: 1em;">
+      <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow-lg bg-white">
+        <table id="example" class="stripe hover " style="width:100%">
           <thead>
             <tr>
               <th data-priority="1">Name</th>
@@ -169,13 +174,13 @@
       </div>
       <!--/Card-->
 
-
-      <button class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full mt-2" id="button">
+      {{-- Disabled FOR DESIGN PURPOSES --}}
+      {{-- <button class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full mt-2" id="button">
         Add Income
       </button><span><button class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full mt-2 ml-2"
           id="button">
           Add Expense
-        </button></button></span>
+        </button></button></span> --}}
     </div>
 
     <!-- jQuery -->
