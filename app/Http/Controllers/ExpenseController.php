5c08c37cbb751ac2a217f4d84ce4ced9c464091b
gelai05 <angelamaemorales1105@gmail.com>
2023-01-28 21:52:56 +0800

    formatting

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -9,6 +9,8 @@ class ExpenseController extends Controller
 {
     public function displayExpense()
     {
-        return view('dashboard.dpages.expenses')->with('expenses', Expense::orderByDesc('created_at')->get())->with('totalexpense', Expense::sum('amount'));
+        return view('dashboard.dpages.expenses')
+        ->with('expenses', Expense::orderByDesc('created_at')->get())
+        ->with('totalexpense', Expense::sum('amount'));
     }
 }
