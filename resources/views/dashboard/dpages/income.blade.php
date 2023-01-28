201847506e0b798761a341578048a2150b9a78d2
gelai05 <angelamaemorales1105@gmail.com>
2023-01-28 23:47:07 +0800

    route

--- resources/views/dashboard/dpages/income.blade.php
+++ resources/views/dashboard/dpages/income.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.maindashboard')
 @section('content')
+
 <head>
 
   <link rel="stylesheet" href="{{ asset('table.css') }}">
@@ -21,74 +22,74 @@
   </button>
 </div>
 <div class="flex flex-wrap">
-    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
-      <!--Metric Card-->
-      <div class=" bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl
+  <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+    <!--Metric Card-->
+    <div class=" bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl
       p-5">
-        <div class="flex flex-row items-center">
-          <div class="flex-shrink pr-4">
-            <div class="rounded-full p-5 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
-          </div>
-          <div class="flex-1 text-right md:text-center">
-            <h5 class="font-bold uppercase text-gray-600">Total Income</h5>
-            <h3 class="font-bold text-3xl"><i class="fa-sharp fa-solid fa-peso-sign"></i>{{$totalincome}}</h3>
-          </div>
+      <div class="flex flex-row items-center">
+        <div class="flex-shrink pr-4">
+          <div class="rounded-full p-5 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
+        </div>
+        <div class="flex-1 text-right md:text-center">
+          <h5 class="font-bold uppercase text-gray-600">Total Income</h5>
+          <h3 class="font-bold text-3xl"><i class="fa-sharp fa-solid fa-peso-sign"></i>{{$totalincome}}</h3>
         </div>
       </div>
     </div>
-
-
-  
-<div class="container w-full md:w-4/5 xl:w-3/5" style="width:100%">
-  <!--Card-->
-  <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
-    <table id="example" class="stripe hover  md:w-4/5 xl:w-3/5">
-      <thead>
-        <tr>
-          <th data-priority="1">Name</th>
-          <th data-priority="2">Description</th>
-          <th data-priority="3">Amount</th>
-          <th data-priority="5">Actions</th>
-        </tr>
-      </thead>
-      <tbody>
-        @foreach($incomes as $income)
-        <tr>
-          <td>{{$income->name}}</td>
-          <td>{{$income->description}}</td>
-          <td><i class="fa-sharp fa-solid fa-peso-sign"></i>{{$income->amount}}</td>
-
-          <td> <a href="{{route('delete/{id}')}}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">🗑️ Delete</a>
-            <a href="{{route('overview')}}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">⚙️ Edit</a>
-          </td>
-
-        </tr>
-        @endforeach
-      </tbody>
-    </table>
   </div>
-  <!--/Card-->
-</div>
 
-<!-- jQuery -->
-<script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
-<!--Datatables -->
-<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
-<script>
-  $(document).ready(function() {
-    var table = $('#example').DataTable({
-      responsive: true,
-      lengthChange: false,
-      pageLength: 5
+
+  <div class="container w-full md:w-4/5 xl:w-3/5" style="width:100%">
+    <!--Card-->
+    <div id='recipients' class="p-8 mt-6 lg:mt-0 rounded shadow bg-white">
+      <table id="example" class="stripe hover  md:w-4/5 xl:w-3/5">
+        <thead>
+          <tr>
+            <th data-priority="1">Name</th>
+            <th data-priority="2">Description</th>
+            <th data-priority="3">Amount</th>
+            <th data-priority="5">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          @foreach($incomes as $income)
+          <tr>
+            <td>{{$income->name}}</td>
+            <td>{{$income->description}}</td>
+            <td><i class="fa-sharp fa-solid fa-peso-sign"></i>{{$income->amount}}</td>
+            <td>
+            <a href="{{route('deleteincome',$income->id)}}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">🗑️ Delete</a>
+              
+              <a href="{{route('overview')}}" class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">⚙️ Edit</a>
+            </td>
+          </tr>
+          @endforeach
+        </tbody>
+      </table>
+    </div>
+    <!--/Card-->
+  </div>
+
+  <!-- jQuery -->
+  <script type="text/javascript" src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+  <!--Datatables -->
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      var table = $('#example').DataTable({
+        responsive: true,
+        lengthChange: false,
+        pageLength: 5
+      });
+
+      $('#entriesPerPage').on('change', function() {
+        table.page.len(this.value).draw();
+      });
     });
-
-    $('#entriesPerPage').on('change', function() {
-      table.page.len(this.value).draw();
-    });
-  });
-</script>
+  </script>
 
 
-@endsection
+  @endsection
