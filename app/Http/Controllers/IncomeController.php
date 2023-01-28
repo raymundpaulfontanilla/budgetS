a124e429074d3978bebc9a3678011950e972be90
gelai05 <angelamaemorales1105@gmail.com>
2023-01-28 20:32:38 +0800

    incomecontroller

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -3,8 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Income;
 
 class IncomeController extends Controller
 {
-    //
+   public function createincome(Request $request){
+    $incomes=new Income;
+    $incomes->name = $request ->name;
+    $incomes->desciption = $request ->desciption;
+    $incomes->amount = $request ->amount;
+    $incomes->save();
+
+   }
 }
