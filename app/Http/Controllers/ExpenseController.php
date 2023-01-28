488cabaa30440bbb3f27f71eff25d24b39f54cff
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-01-28 21:36:32 +0800

    display expense function

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,9 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Expense;
+
 
 class ExpenseController extends Controller
 {
-    //
+    public function displayExpense()
+    {
+        return view('dashboard.dpages.expenses')->with('expenses', Expense::orderByDesc('created_at')->get())->with('totalexpense', Expense::sum('amount'));
+    }
 }
