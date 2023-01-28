682da9af7d92a14dd18eeedbd5de4fec1e180ff2
gelai05 <angelamaemorales1105@gmail.com>
2023-01-28 22:23:51 +0800

    added icon

--- resources/views/dashboard/dpages/expense.blade.php
+++ resources/views/dashboard/dpages/expense.blade.php
@@ -20,6 +20,26 @@
   <button class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full mt-5" id="button">
     Add Expenses
   </button>
+
+  <div class="flex flex-wrap">
+    <div class="w-full md:w-1/2 xl:w-1/3 p-6">
+      <!--Metric Card-->
+      <div class=" bg-gradient-to-b from-indigo-200 to-indigo-100 border-b-4 border-indigo-500 rounded-lg shadow-xl
+      p-5">
+        <div class="flex flex-row items-center">
+          <div class="flex-shrink pr-4">
+            <div class="rounded-full p-5 bg-indigo-600"><i class="fas fa-tasks fa-2x fa-inverse"></i></div>
+          </div>
+          <div class="flex-1 text-right md:text-center">
+            <h5 class="font-bold uppercase text-gray-600">Total Income</h5>
+            <h3 class="font-bold text-3xl"><i class="fa-sharp fa-solid fa-peso-sign"></i>{{$totalexpense}}</h3>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+
 </div>
 <div class="container w-full md:w-4/5 xl:w-3/5" style="width:100%">
   <!--Card-->
@@ -36,9 +56,9 @@
       <tbody>
         @foreach($expenses as $expense)
         <tr>
-          <td>{{$expense->name}}</td>
-          <td>{{$expense->description}}</td>
-          <td>{{$expense->amount}}</td>
+          <td><i class="fa-sharp fa-solid fa-peso-sign">{{$expense->name}}</td>
+          <td><i class="fa-sharp fa-solid fa-peso-sign">{{$expense->description}}</td>
+          <td><i class="fa-sharp fa-solid fa-peso-sign">{{$expense->amount}}</td>
           <td> <a href="{{route('history')}}"
               class="flex items-center focus:outline-none focus:ring-2 focus:ring-white">🗑️ Delete</a>
             <a href="{{route('overview')}}"
