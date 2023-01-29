f8784b4262233408b7fad2b3dfa5b9039b6e07db
gelai05 <angelamaemorales1105@gmail.com>
2023-01-29 11:15:51 +0800

    added create expense function

--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
 use App\Models\Expense;
 
 
@@ -12,6 +13,18 @@ class ExpenseController extends Controller
         return view('dashboard.dpages.expense')
             ->with('expenses', Expense::orderByDesc('created_at')->get())
             ->with('totalexpense', Expense::sum('amount'));
+    }
+
+    public function createexpense(Request $request)
+    {
+        $expenses = new Expense;
+        $expenses->name = $request->name;
+        $expenses->description = $request->description;
+        $expenses->amount = $request->amount;
+        $expenses->save();
+
+        return redirect()->route('expense');
+
     }
 
     public function deleteexpense($id)
