4d6d2e0a43f40c1d267d0609eff2b30eb0d35d81
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-01-31 11:07:49 +0800

    refactor double semicolon into single

--- app/Http/Controllers/TransactionHistoryController.php
+++ app/Http/Controllers/TransactionHistoryController.php
@@ -7,12 +7,13 @@ use App\Models\Expense;
 
 class TransactionHistoryController extends Controller
 {
-    public function transactionhistory(){
+    public function transactionhistory()
+    {
 
-        $income = Income::all();;
+        $income = Income::all();
         $expense = Expense::all();
         $merges = $income->concat($expense);
         return view('dashboard.dpages.transactionhistory')
-        ->with('merges',$merges);
+            ->with('merges', $merges);
     }
 }
