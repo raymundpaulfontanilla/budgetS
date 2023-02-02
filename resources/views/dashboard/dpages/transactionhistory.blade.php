9ae1a44685fd165cc9f0eed038ffc6652a58799a
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-02 14:22:51 +0800

    change button tag to a tag for routings

--- resources/views/dashboard/dpages/transactionhistory.blade.php
+++ resources/views/dashboard/dpages/transactionhistory.blade.php
@@ -28,9 +28,9 @@
     <div class="container w-full md:w-4/5 xl:w-3/5 mt-5" style="width:100%">
         <!--Card-->
         <div id='recipients' class="p-2 mt-6 lg:mt-0 rounded shadow bg-white mr-6">
-            <button href={{route('generatepdf')}} target="_blank"
+            <a href={{route('generatepdf')}} target="_blank"
                 class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full">PDF
-                BUTTON</button>
+                BUTTON</a>
                 <a href={{route('sendemail')}}
                 class="bg-teal-400 hover:bg-teal-600 text-white font-bold py-2 px-4 rounded-full">Send
                 Email</a>
