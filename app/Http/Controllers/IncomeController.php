a41dbf23d21490f12df3523f9eb4a918ecc4f633
gelai05 <angelamaemorales1105@gmail.com>
2023-02-08 17:33:27 +0800

    modified monthly line graph for income

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -9,6 +9,18 @@ class IncomeController extends Controller
 {
     public function displayincome()
     {
+        $january = Income::whereRaw("MONTH(created_at) = 1")->sum('amount');
+        $february = Income::whereRaw("MONTH(created_at) = 2")->sum('amount');
+        $march = Income::whereRaw("MONTH(created_at) = 3")->sum('amount');
+        $april = Income::whereRaw("MONTH(created_at) = 4")->sum('amount');
+        $may = Income::whereRaw("MONTH(created_at) = 5")->sum('amount');
+        $june = Income::whereRaw("MONTH(created_at) = 6")->sum('amount');
+        $july = Income::whereRaw("MONTH(created_at) = 7")->sum('amount');
+        $august = Income::whereRaw("MONTH(created_at) = 8")->sum('amount');
+        $september = Income::whereRaw("MONTH(created_at) = 9")->sum('amount');
+        $october = Income::whereRaw("MONTH(created_at) = 10")->sum('amount');
+        $november = Income::whereRaw("MONTH(created_at) = 11")->sum('amount');
+        $december = Income::whereRaw("MONTH(created_at) = 12")->sum('amount');
         $monday = Income::whereRaw("WEEKDAY(created_at) = 0")->sum('amount');
         $tuesday = Income::whereRaw("WEEKDAY(created_at) = 1")->sum('amount');
         $wednesday = Income::whereRaw("WEEKDAY(created_at) = 2")->sum('amount');
@@ -16,7 +28,7 @@ class IncomeController extends Controller
         $friday = Income::whereRaw("WEEKDAY(created_at) = 4")->sum('amount');
         $saturday = Income::whereRaw("WEEKDAY(created_at) = 5")->sum('amount');
         $sunday = Income::whereRaw("WEEKDAY(created_at) = 6")->sum('amount');
-        return view('dashboard.dpages.income', compact('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'))
+        return view('dashboard.dpages.income', compact('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday','january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'))
             ->with('incomes', Income::orderByDesc('created_at')->get())
             ->with('totalincome', Income::sum('amount'));
 
