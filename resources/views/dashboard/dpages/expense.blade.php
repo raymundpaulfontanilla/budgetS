53605383df4bf6470ef884336f8f7446e5c87990
Zafe123 <scottpilgrim712@gmail.com>
2023-02-08 16:34:49 +0800

    Fixed issues in expense and income

--- resources/views/dashboard/dpages/expense.blade.php
+++ resources/views/dashboard/dpages/expense.blade.php
@@ -74,122 +74,125 @@
                   data-modal-toggle="deletemodal{{ $expense->id }}" type="button">
                   🗑️ Delete</button>
               </td>
+              {{-- Delete Modal --}}
+              <div id="deletemodal{{ $expense->id }}" tabindex="-1"
+                class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                <div class="relative w-full h-full max-w-md md:h-auto">
+                  <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
+                    <button type="button"
+                      class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                      data-modal-hide="deletemodal{{ $expense->id }}">
+                      <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                          clip-rule="evenodd"></path>
+                      </svg>
+                      <span class="sr-only">Close modal</span>
+                    </button>
+                    <div class="p-6 text-center">
+                      <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
+                        fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                          d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                      </svg>
+                      <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to
+                        remove
+                        this
+                        record?</h3>
+                      <a href="{{route('deleteexpense',$expense->id)}}"><button data-modal-hide="popup-modal"
+                          type="button"
+                          class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                          Yes, I'm sure
+                        </button></a>
+                      <button data-modal-hide="deletemodal{{ $expense->id }}" type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
+                        cancel</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {{-- Edit Modal --}}
+              <div id="editmodal{{ $expense->id }}" data-modal-backdrop="static" tabindex="-1"
+                class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
+                <div class="relative w-full h-full max-w-lg md:h-auto">
+                  <!-- Modal content -->
+                  <div class="relative bg-white rounded-lg shadow bg-emerald-300">
+                    <!-- Modal header -->
+                    <div class=" flex items-center justify-between p-5 border-b rounded-t border-white">
+                      <h3 class="text-xl font-medium text-black">
+                        Edit Form
+                      </h3>
+                      <button type="button"
+                        class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                        data-modal-hide="editmodal{{ $expense->id }}">
+                        <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path fill-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd"></path>
+                        </svg>
+                        <span class="sr-only">Close modal</span>
+                      </button>
+                    </div>
+                    <!-- Modal body -->
+                    <div class="px-6 py-6 lg:px-8">
+                      <form class="space-y-6" method="POST" action="{{route('editexpense',$expense->id)}}">
+                        @csrf
+                        <div>
+                          <div>
+                            <label for="editexpensename" class="block mb-2 text-base font-medium text-black">Select
+                              Income
+                              Category</label>
+                            <select id="editexpensename" name="name" value="{{$expense->name}}"
+                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                              <option value="Baby">Baby</option>
+                              <option value="Beauty">Beauty</option>
+                              <option value="Bills">Bills</option>
+                              <option value="Car">Car</option>
+                              <option value="Clothing">Clothing</option>
+                              <option value="Education">Education</option>
+                              <option value="Entertainment">Entertainment</option>
+                              <option value="Food">Food</option>
+                              <option value="Health">Health</option>
+                              <option value="Home">Home</option>
+                              <option value="Insurance">Insurance</option>
+                              <option value="Shopping">Shopping</option>
+                              <option value="Social">Social</option>
+                              <option value="Sport">Sport</option>
+                              <option value="Tax">Tax</option>
+                              <option value="Telephone">Telephone</option>
+                              <option value="Transportation">Transportation</option>
+                            </select>
+                          </div>
+                        </div>
+                        <div>
+                          <label class="block mb-2 text-base font-medium text-black">Description</label>
+                          <input type="text" name="description" id="description" value="{{$expense->description}}"
+                            class="bg-gray-200 border border-black text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-black dark:placeholder-gray-200 dark:text-black"
+                            required />
+                        </div>
+                        <div>
+                          <label class="block text-base font-medium text-black">Amount</label>
+                          <input type="text" name="amount" id="amount" value="{{$expense->amount}}"
+                            class="bg-gray-200 border border-black text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-black dark:placeholder-gray-200 dark:text-black"
+                            required />
+                        </div>
+                    </div>
+                    <!-- Modal footer -->
+                    <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b border-white">
+                      <button data-modal-hide="" type="submit" href="{{route('editexpense',$expense->id)}}"
+                        class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+                      <button data-modal-hide="editmodal{{ $expense->id }}" type="button"
+                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
+                    </div>
+                  </div>
+                  </form>
+                </div>
+              </div>
             </tr>
+            @endforeach
           </tbody>
-          {{-- Delete Modal --}}
-          <div id="deletemodal{{ $expense->id }}" tabindex="-1"
-            class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-            <div class="relative w-full h-full max-w-md md:h-auto">
-              <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button type="button"
-                  class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                  data-modal-hide="deletemodal{{ $expense->id }}">
-                  <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clip-rule="evenodd"></path>
-                  </svg>
-                  <span class="sr-only">Close modal</span>
-                </button>
-                <div class="p-6 text-center">
-                  <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none"
-                    stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
-                  </svg>
-                  <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to remove
-                    this
-                    record?</h3>
-                  <a href="{{route('deleteexpense',$expense->id)}}"><button data-modal-hide="popup-modal" type="button"
-                      class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
-                      Yes, I'm sure
-                    </button></a>
-                  <button data-modal-hide="deletemodal{{ $expense->id }}" type="button"
-                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                    cancel</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          {{-- Edit Modal --}}
-          <div id="editmodal{{ $expense->id }}" data-modal-backdrop="static" tabindex="-1"
-            class="fixed top-0 left-0 right-0 z-50 hidden w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full">
-            <div class="relative w-full h-full max-w-lg md:h-auto">
-              <!-- Modal content -->
-              <div class="relative bg-white rounded-lg shadow bg-emerald-300">
-                <!-- Modal header -->
-                <div class=" flex items-center justify-between p-5 border-b rounded-t border-white">
-                  <h3 class="text-xl font-medium text-black">
-                    Edit Form
-                  </h3>
-                  <button type="button"
-                    class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-600 dark:hover:text-white"
-                    data-modal-hide="editmodal{{ $expense->id }}">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
-                      xmlns="http://www.w3.org/2000/svg">
-                      <path fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd"></path>
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                  </button>
-                </div>
-                <!-- Modal body -->
-                <div class="px-6 py-6 lg:px-8">
-                  <form class="space-y-6" method="POST" action="{{route('editexpense',$expense->id)}}">
-                    @csrf
-                    <div>
-                      <div>
-                        <label for="editexpensename" class="block mb-2 text-base font-medium text-black">Select Income
-                          Category</label>
-                        <select id="editexpensename" name="name" value="{{$expense->name}}"
-                          class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                          <option value="Baby">Baby</option>
-                          <option value="Beauty">Beauty</option>
-                          <option value="Bills">Bills</option>
-                          <option value="Car">Car</option>
-                          <option value="Clothing">Clothing</option>
-                          <option value="Education">Education</option>
-                          <option value="Entertainment">Entertainment</option>
-                          <option value="Food">Food</option>
-                          <option value="Health">Health</option>
-                          <option value="Home">Home</option>
-                          <option value="Insurance">Insurance</option>
-                          <option value="Shopping">Shopping</option>
-                          <option value="Social">Social</option>
-                          <option value="Sport">Sport</option>
-                          <option value="Tax">Tax</option>
-                          <option value="Telephone">Telephone</option>
-                          <option value="Transportation">Transportation</option>
-                        </select>
-                      </div>
-                    </div>
-                    <div>
-                      <label class="block mb-2 text-base font-medium text-black">Description</label>
-                      <input type="text" name="description" id="description" value="{{$expense->description}}"
-                        class="bg-gray-200 border border-black text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-black dark:placeholder-gray-200 dark:text-black"
-                        required />
-                    </div>
-                    <div>
-                      <label class="block text-base font-medium text-black">Amount</label>
-                      <input type="text" name="amount" id="amount" value="{{$expense->amount}}"
-                        class="bg-gray-200 border border-black text-gray-500 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-100 dark:border-black dark:placeholder-gray-200 dark:text-black"
-                        required />
-                    </div>
-                </div>
-                <!-- Modal footer -->
-                <div class="flex items-center p-6 space-x-2 border-t border-gray-200 rounded-b border-white">
-                  <button data-modal-hide="" type="submit" href="{{route('editexpense',$expense->id)}}"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
-                  <button data-modal-hide="editmodal{{ $expense->id }}" type="button"
-                    class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Cancel</button>
-                </div>
-              </div>
-              </form>
-            </div>
-          </div>
-          @endforeach
         </table>
       </div>
       <!--/Card-->
