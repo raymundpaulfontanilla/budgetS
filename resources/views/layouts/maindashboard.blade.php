860428bbcc265dc561de9ebe049d95ee0e9bb847
gelai05 <angelamaemorales1105@gmail.com>
2023-02-09 13:46:29 +0800

    added routes in main dashboard

--- resources/views/layouts/maindashboard.blade.php
+++ resources/views/layouts/maindashboard.blade.php
@@ -97,7 +97,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="" class="flex items-center p-2 font-medium text-black hover:text-white cursor-pointer">
+                        <a href="{{route('userprofile')}}" class="flex items-center p-2 font-medium text-black hover:text-white cursor-pointer">
                             <svg aria-hidden="true" class="flex-shrink-0 w-6 h-6" fill="currentColor"
                                 viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
                                 <path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
