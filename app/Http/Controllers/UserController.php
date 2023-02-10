43d3a1fe5626bc0880dabd3ce0ce77883b74b9c8
gelai05 <angelamaemorales1105@gmail.com>
2023-02-10 19:07:10 +0800

    modified usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,35 +1,36 @@
 <?php
 
 namespace App\Http\Controllers;
-use app\Models\User;
-use Illuminate\Support\Facades\Hash;
+
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     public function displayuserprofile()
     {
         $user = Auth::user();
-        return view('dashboard.dpages.userprofile', ['name' => $user->name, 'email' => $user->email,'password' => $user->password]);
+        return view('dashboard.dpages.userprofile', ['name' => $user->name, 'email' => $user->email, 'password' => $user->password]);
     }
 
     public function edituserprofile(Request $request)
-{
-    $user = Auth::user();
 
-    $request->validate([
-        'password' => 'required|confirmed',
-    ]);
-
-    $user->name = $request->input('name');
-    $user->email = $request->input('email');
-    if ($request->input('password')) {
-        $user->password = Hash::make($request->input('password'));
+    {
+        $user = Auth::user();
+        if (Hash::check($request->input('password'), $user->password)) {
+            $user->name = $request->input('name');
+            $user->email = $request->input('email');
+            $user->password = Hash::make($request->input('password'));
+            $user->save();
+            return redirect()->route('userprofile');
+        } else {
+            return redirect()->route('userprofile')->with('error', 'Password is Incorrect');
+        }
     }
-    $user->save();
-    return redirect()->route('userprofile');
-}
+
+   
 }
 
 
