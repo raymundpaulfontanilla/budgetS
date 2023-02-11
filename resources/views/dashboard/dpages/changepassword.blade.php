bed13e8707192ddfcf2339b884983720dc28af7e
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-11 13:16:45 +0800

    refactored functionalities including tailwind

--- resources/views/dashboard/dpages/changepassword.blade.php
+++ resources/views/dashboard/dpages/changepassword.blade.php
@@ -1,63 +1,42 @@
 @extends('layouts.maindashboard')
 @section('content')
 
-<div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Change password</div>
-                    <div class="card-body">
-                        @if (session('error'))
-                            <div class="alert alert-danger">
-                                {{ session('error') }}
-                            </div>
-                        @endif
-                        @if (session('success'))
-                            <div class="alert alert-success">
-                                {{ session('success') }}
-                            </div>
-                        @endif
-                        <form class="form-horizontal" method="POST" action="{{ route('updatepassword') }}">
-                            {{ csrf_field() }}
-                            <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
-                                <label for="new-password" class="col-md-4 control-label">Current Password</label>
-                                <div class="col-md-6">
-                                    <input id="current-password" type="password" class="form-control" name="current-password" required>
-                                    @if ($errors->has('current-password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('current-password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
-                                <label for="new-password" class="col-md-4 control-label">New Password</label>
-                                <div class="col-md-6">
-                                    <input id="new-password" type="password" class="form-control" name="new-password" required>
-                                    @if ($errors->has('new-password'))
-                                        <span class="help-block">
-                                            <strong>{{ $errors->first('new-password') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="new-password-confirm" class="col-md-4 control-label">Confirm New Password</label>
-                                <div class="col-md-6">
-                                    <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation" required>
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <div class="col-md-6 col-md-offset-4">
-                                    <button type="submit" class="btn btn-primary">
-                                        Change Password
-                                    </button>
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+<div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700 place-content-center">
+    @if (session('error'))
+        <div class="alert alert-danger">
+            {{ session('error') }}
         </div>
-    </div>
+    @endif
+    @if (session('success'))
+        <div class="alert alert-success">
+            {{ session('success') }}
+        </div>
+    @endif
+    <form method="POST" method="POST" action="{{ route('updatepassword') }}">
+        @csrf
+        <div class="card-header">Change password</div>
+          <div class="relative z-0 w-full mb-6 group {{ $errors->has('current-password') ? ' has-error' : '' }}">
+              <input type="password" name="current-password" id="current-password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+              @if ($errors->has('current-password'))
+              <span class="help-block">
+                  <strong>{{ $errors->first('current-password') }}</strong>
+              </span>
+              @endif
+              <label for="new-password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Current Password</label>
+          </div>
+          <div class="relative z-0 w-full mb-6 group {{ $errors->has('new-password') ? ' has-error' : '' }}">
+              <input type="password" name="new-password" id="new-password" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" required />
+              @if ($errors->has('new-password'))
+                <span class="help-block">
+                    <strong>{{ $errors->first('new-password') }}</strong>
+                </span>
+              @endif
+              <label for="new-password" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">New Password</label>
+          </div>
+          <div class="relative z-0 w-full mb-6 group">
+              <input type="password" name="new-password_confirmation" id="new-password-confirm" class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" placeholder=" " required />
+              <label for="new-password-confirm" class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm New Password</label>
+              <button type="submit" class="mt-1 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Change Password</button>
+      </form>
+  </div>
 @endsection
