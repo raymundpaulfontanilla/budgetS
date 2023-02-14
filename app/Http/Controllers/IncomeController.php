660022e9a3c6ef1d42c7261ab40e9280c2666d89
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-14 13:20:42 +0800

    added eloquent relationship for income controller

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -9,34 +9,34 @@ class IncomeController extends Controller
 {
     public function displayincome()
     {
-        $january = Income::whereRaw("MONTH(created_at) = 1")->sum('amount');
-        $february = Income::whereRaw("MONTH(created_at) = 2")->sum('amount');
-        $march = Income::whereRaw("MONTH(created_at) = 3")->sum('amount');
-        $april = Income::whereRaw("MONTH(created_at) = 4")->sum('amount');
-        $may = Income::whereRaw("MONTH(created_at) = 5")->sum('amount');
-        $june = Income::whereRaw("MONTH(created_at) = 6")->sum('amount');
-        $july = Income::whereRaw("MONTH(created_at) = 7")->sum('amount');
-        $august = Income::whereRaw("MONTH(created_at) = 8")->sum('amount');
-        $september = Income::whereRaw("MONTH(created_at) = 9")->sum('amount');
-        $october = Income::whereRaw("MONTH(created_at) = 10")->sum('amount');
-        $november = Income::whereRaw("MONTH(created_at) = 11")->sum('amount');
-        $december = Income::whereRaw("MONTH(created_at) = 12")->sum('amount');
-        $monday = Income::whereRaw("WEEKDAY(created_at) = 0")->sum('amount');
-        $tuesday = Income::whereRaw("WEEKDAY(created_at) = 1")->sum('amount');
-        $wednesday = Income::whereRaw("WEEKDAY(created_at) = 2")->sum('amount');
-        $thursday = Income::whereRaw("WEEKDAY(created_at) = 3")->sum('amount');
-        $friday = Income::whereRaw("WEEKDAY(created_at) = 4")->sum('amount');
-        $saturday = Income::whereRaw("WEEKDAY(created_at) = 5")->sum('amount');
-        $sunday = Income::whereRaw("WEEKDAY(created_at) = 6")->sum('amount');
+        $user = auth()->user();
+        $january = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 1")->sum('amount');
+        $february = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 2")->sum('amount');
+        $march = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 3")->sum('amount');
+        $april = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 4")->sum('amount');
+        $may = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 5")->sum('amount');
+        $june = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 6")->sum('amount');
+        $july = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 7")->sum('amount');
+        $august = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 8")->sum('amount');
+        $september = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 9")->sum('amount');
+        $october = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 10")->sum('amount');
+        $november = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 11")->sum('amount');
+        $december = Income::where('user_id', $user->id)->whereRaw("MONTH(created_at) = 12")->sum('amount');
+        $monday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 0")->sum('amount');
+        $tuesday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 1")->sum('amount');
+        $wednesday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 2")->sum('amount');
+        $thursday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 3")->sum('amount');
+        $friday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 4")->sum('amount');
+        $saturday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 5")->sum('amount');
+        $sunday = Income::where('user_id', $user->id)->whereRaw("WEEKDAY(created_at) = 6")->sum('amount');
+        $incomes = Income::where('user_id', $user->id)->get();
+        $totalincome = Income::where('user_id', $user->id)->sum('amount');
         return view('dashboard.dpages.income', compact('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday', 'january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december'))
-            ->with('incomes', Income::orderByDesc('created_at')->get())
-            ->with('totalincome', Income::sum('amount'));
-
+            ->with('incomes', $incomes)
+            ->with('totalincome', $totalincome);
     }
-
     public function createincome(Request $request)
     {
-
 
         $incomes = new Income;
         $incomes->name = $request->name;
@@ -45,10 +45,11 @@ class IncomeController extends Controller
         $request->validate([
             'amount' => 'required|numeric',
         ]);
+        $incomes->user_id = auth()->id();
         $incomes->save();
         $request->session()->flash('income', 'Income Recorded Successfully!');
         return redirect()->route('income');
-
+        
 
     }
 
