9052ed46052332d65f2cc9528d43c3aca17a3fd7
raymundpaulfontanilla <raymundpaulfontanilla@mgmail.com>
2023-02-14 13:20:30 +0800

    added eloquent relationship for pdf controller

--- app/Http/Controllers/GeneratePDFController.php
+++ app/Http/Controllers/GeneratePDFController.php
@@ -9,9 +9,9 @@ class GeneratePDFController extends Controller
 {
     public function generatepdf()
     {
-
-        $income = Income::all();
-        $expense = Expense::all();
+        $user = auth()->user();
+        $income = Income::where('user_id', $user->id)->get();
+        $expense = Expense::where('user_id', $user->id)->get();
         $merges = $income->concat($expense);
         $data = [
             'merges' => $merges,
